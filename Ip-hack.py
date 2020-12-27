a207223edd1660e8288fb30555bb3e11cf01fec6
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-27 14:03:09 +0800

    Update Ip-hack.py

--- Ip-hack.py
+++ Ip-hack.py
@@ -43,6 +43,8 @@ print
 print
 print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mM\033[32uhammad \033[33mP\033[32Raihan\033[0m"
 time.sleep(1)
+print "\033[1m\033[33m\n\tWhatshapp :  082331072836
+time.sleep(1)
 print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mMR404cyber\033[0m"
 time.sleep(1)
 print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
