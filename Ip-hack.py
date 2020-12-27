6426776b2e2232d7e4d295017d7d66b82aefa44c
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-27 14:26:28 +0800

    Update Ip-hack.py

--- Ip-hack.py
+++ Ip-hack.py
@@ -23,35 +23,6 @@
 #
 #
 
-import sys
-import time
-import os
-from urllib2 import *
-from platform import system
-print
-print
-print
-os.system("clear")
-os.system("bash logo.sh")
-print
-print
-print
-print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m Author Info \033[33m]\033[31m==>\033[0m"
-time.sleep(3)
-print
-print
-print
-print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mM\033[32uhammad \033[33mP\033[32Raihan\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tWhatshapp :  \033[32m \033[33mMR\033[32m.404 \033[33m0\033[32m82331072836\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mMR404cyber\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
-time.sleep(2)
-print
-print
-
 def ping():
 	print
 	print
